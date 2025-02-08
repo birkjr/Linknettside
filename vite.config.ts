ac456c780a0c797412b0562e229dc3bf704960fc
birkjr <birkrams@gmail.com>
2025-02-08 18:10:03 +0100

    Fixed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-})
-
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: "dist"
+  },
+  base: "/",
+});
