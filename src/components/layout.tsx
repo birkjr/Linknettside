6ab29a9e1e51ec8bca6cd074d4657eaac84251f1
Birk Jonathan Ramstad <birkrams@gmail.com>
2025-10-07 15:05:38 +0200

    Fix mobile scroll issues - remove sticky positioning on mobile

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,9 +4,9 @@ import Footer from './Footer';
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow pb-4">
         <Outlet /> {/* This renders the current page */}
       </main>
       <Footer />
